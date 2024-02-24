e15ff6251362fadb77cd2d14103a2cd97f1cdb03
seyi abejide <abejidemustapha@gmail.com>
2024-02-24 04:48:21 -0800

    second

---
+++ .history/app/[category]/page_20240224042748.tsx
@@ -0,0 +1,76 @@
+import { client } from '@/sanity/lib/client'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+async function getCat(category: string){
+    const query = `*[_type == "product" && category->name == "${category}"]{
+  
+           
+          _id,
+          price,
+          name,
+          "slug": slug.current,
+          "category": category -> name,
+          "images": images[0].asset->url
+    }
+
+    `
+    
+    const data = await client.fetch(query)
+   
+    return data
+}
+
+async function page({params,}: {params: {category: string}}) {
+    const datas = await getCat(params.category)
+    console.log(datas)
+
+  return (
+    <div className="bg-[#e6e6e6] mt-24 p-3">   
+                                                                                                                                                                                                                                                                                  
+    <div className="">
+      <div className="flex justify-center text-center items-center">
+        <h2 className="text-2xl italic bg-green-400 px-2 py-2 flex text-center font-bold tracking-tight text-gray-900">
+           {params.category} Section
+        </h2>
+      </div>
+
+      <div className="mt-6 border-1 grid grid-cols-1    gap-2 sm:grid-cols-2 lg:grid-cols-4  ">
+        {datas.map((p: any) => (
+          <div key={p._id} className="p-3 items-center ">
+            <div className="  object-contain rounded-lg">
+              <Image
+                src={p.images}
+                alt="Product image"
+                width={100}
+                height={100}
+               
+                
+                
+               
+              />
+            </div>
+
+            <div className="mt-2 flex flex-col">
+              <div>
+                <h1 className="text-sm text-gray-700">
+                  <Link className='flex flex-col space-y-1' href={`/product/${p.slug}`}>
+                   <h2> {p.name}</h2>
+                    <span>view </span>
+                  </Link>
+                </h1>
+               
+              </div>
+              <p className="text-sm font-medium text-gray-900">
+                ${p.price}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default page
